a1133f694bddd53d82ae2e675c4c5a54f24a5b03
MontebellerZ <vyatrhjar@gmail.com>
2024-03-26 09:39:28 -0300

    remove unnecessary insertion method

--- agrupamento_back/src/functions/binarySearch.ts
+++ agrupamento_back/src/functions/binarySearch.ts
@@ -16,25 +16,6 @@ export function binarySearch(search: number, sorted: number[]) {
     return min;
 }
 
-export function insertSortedPoint(obj: Point, sorted: Point[]) {
-    if (sorted.length <= 0 || obj.value >= sorted[sorted.length - 1].value) {
-        sorted.push(obj);
-        return sorted;
-    }
-
-    if (obj.value <= sorted[0].value) {
-        sorted.unshift(obj);
-        return sorted;
-    }
-
-    const insertPos = binarySearch(
-        obj.value,
-        sorted.map(({ value }) => value)
-    );
-    sorted.splice(insertPos, 0, obj);
-    return sorted;
-}
-
 export function insertSortedNumber(n: number, sorted: number[]) {
     const insertPos = binarySearch(n, sorted);
     sorted.splice(insertPos, 0, n);
